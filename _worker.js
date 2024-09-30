58f5f4bb263f32501811b39b1427e8786fdc5bd5
siren202101 <80493699+siren202101@users.noreply.github.com>
2024-09-30 20:38:56 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -286,8 +286,8 @@ export default {
                         });
 
                     default:
-                        // return new Response('Not found', { status: 404 });
-                        url.hostname = 'edgetunnel.anycast.eu.org';
+                        return new Response('Not found', { status: 404 });
+                        url.hostname = 'www.speedtest.net';
                         url.protocol = 'https:';
                         request = new Request(url, request);
                         return await fetch(request);
